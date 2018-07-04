c1d812c511692b0164c7a44cb76a24db6960d411
Vinicius Pereira <vinicius@localhost.localdomain>
2018-07-04 11:01:09 -0300

    push in configuration files

---
+++ client/main.js
@@ -0,0 +1,55 @@
+import {Template} from 'meteor/templating';
+import {ReactiveVar} from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+    // counter starts at 0
+    this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+    counter() {
+        return Template.instance().counter.get();
+    },
+});
+
+Template.hello.events({
+    'click button'(event, instance) {
+        // increment the counter when button is clicked
+        instance.counter.set(instance.counter.get() + 1);
+    },
+});
+
+Template.SON.helpers({
+    hello() {
+        return 'Hello World Vinic';
+    },
+    // list() {
+    //   return [{
+    //       name: "Marcos",
+    //       lastname: "Vinicius"
+    //   }, {
+    //       name: "Victoria",
+    //       lastname: "Maria"
+    //   }, {
+    //       name: "Maria",
+    //       lastname: "Vitoria"
+    //   }]
+    // }
+});
+
+Template.body.helpers({
+    list() {
+        return [{
+            name: "Marcos",
+            lastname: "Vinicius"
+        }, {
+            name: "Victoria",
+            lastname: "Maria"
+        }, {
+            name: "Maria",
+            lastname: "Vitoria"
+        }]
+    }
+});
